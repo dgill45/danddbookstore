69a3cd774227e7e6dbf008377a620c75990b9575
dgill45 <donell.gill@outlook.com>
2023-07-26 07:05:56 -0400

    Added description member to product attributes.

--- src/Product.java
+++ src/Product.java
@@ -2,11 +2,13 @@ public class Product {
     private int id;
     private String name;
     private double price;
+    private String desc;
 
-    public Product(int id, String name, double price){
+    public Product(int id, String name, double price, String desc){
         this.id = id;
         this.name = name;
         this.price = price;
+        this.desc = desc;
     }
 
     public int getId() {
--- src/Shoprunner.java
+++ src/Shoprunner.java
@@ -14,11 +14,11 @@ public class Shoprunner {
     public static void main( String[] args) {
 
         ArrayList<Product> products = new ArrayList<>();
-                products.add(new Product(0, "The Knights of Malkillian", 49.99));
-                products.add(new Product(1, "Children of Notgnivri", 24.99));
-                products.add(new Product(2, "Stones of the Three Sisters", 49.99));
-                products.add(new Product(3, "Tales from the Vale", 34.99));
-                products.add(new Product(4, "The Three Elven Towers", 49.99));
+                products.add(new Product(0, "The Knights of Malkillian", 49.99, "Prevent the rise of a warlord returned from the dead to wreak havoc on the kingdom of Storm's Reach."));
+                products.add(new Product(1, "Children of Notgnivri", 24.99, "Investigate the horrible disappearances of a coastal towns children."));
+                products.add(new Product(2, "Stones of the Three Sisters", 49.99, "The City of Three Sisters was blessed by three goddesses after years of hard won independence and charity. Recover the stones stolen from the city and return it to its glory."));
+                products.add(new Product(3, "Tales from the Vale", 34.99, "A collection of short adventures in the wilderness of Arrynvale."));
+                products.add(new Product(4, "The Three Elven Towers", 49.99, "Journey to a land of ancient elven magic and mayhem in three rival cities to stop the recovery of an ancient weapon . . . and an ancient evil."));
 
         Shop shop = new Shop("5e Bookstore", products);
         Menu menu = new Menu(shop, new Scanner(new InputStreamReader(System.in, StandardCharsets.UTF_8)));
